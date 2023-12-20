4161d24cd6cee31a54029b6216bb46b0a57b4b78
dsm2588 <donaldmaze-england@utexas.edu>
2023-12-20 17:54:33 -0600

    Added python script to generate a color bar reference for the class colors using matplot lib

---
+++ generate_colorbar_reference.py
@@ -0,0 +1,33 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+with open('class_palette.txt', 'rt') as pal_file:
+    lines = pal_file.readlines()
+
+hex_colors = []
+for line in lines:
+    hex_colors.append('#' + line.strip())
+
+fig, ax = plt.subplots(figsize=(6, 1))
+fig.subplots_adjust(bottom=0.5)
+
+# Create a LinearSegmentedColormap with the hex colors
+cmap = mpl.colors.LinearSegmentedColormap.from_list("", hex_colors)
+
+# Set boundaries for discrete color segments
+bounds = range(len(hex_colors))  # Boundaries for each color
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+cb2 = mpl.colorbar.ColorbarBase(
+   ax,
+   cmap=cmap,
+   norm=norm,
+   boundaries=bounds,  # Use the same boundaries for ticks
+   extend='neither',  # No extension for discrete colors
+   ticks=bounds,
+   spacing='uniform',  # Uniform spacing for discrete colors
+   orientation='horizontal'
+)
+cb2.set_label('VIPER class color reference')
+fig.show()
+input('Press any key to close...')
